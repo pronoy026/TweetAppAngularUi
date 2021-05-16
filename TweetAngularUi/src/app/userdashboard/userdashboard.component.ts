95bb196960b6ca4073791565658e1e80bf1fa38c
pronoy026 <pronoysarkar16@gmail.com>
2021-05-17 03:20:31 +0530

    user timeline design

--- TweetAngularUi/src/app/userdashboard/userdashboard.component.ts
+++ TweetAngularUi/src/app/userdashboard/userdashboard.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserdashboardComponent implements OnInit {
 
+  payment: any
+  doCommentToggler: boolean
+  seeCommentsToggler: boolean
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleCommentBox() {
+    this.doCommentToggler= !this.doCommentToggler
+    console.log("comment box toggled")
+  }
+
+  postTweet() {
+    console.log("Tweet posted!")
+  }
+
+  seeComments() {
+    this.seeCommentsToggler= !this.seeCommentsToggler
+    console.log("see comments");
+  }
+
+  replyComment() {
+    console.log("replied")
+  }
+
+  toCommentBox() {
+    console.log("To comment box")
+  }
 }
